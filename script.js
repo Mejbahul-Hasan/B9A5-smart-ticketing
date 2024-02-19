285027a47ec8c7ced825e1bd7d94bf8d0a6c9c95
Mejbahul-Hasan <mejba.du@gmail.com>
2024-02-19 23:40:30 +0600

    final sumbission

--- script.js
+++ script.js
@@ -2,43 +2,47 @@ const seats = document.querySelectorAll('.seat');
 
 let count = 1;
 let totalPrice = 550;
+let isSeatSelected = false;
+let count1 = 0;
 
-if(count <= 4){
+
     for(const seat of seats){
         seat.addEventListener('click', function(e){
-            
-            seatColor = seat.classList.add('bg-green-500');
+            if(count1 <= 3){
+                
+                seatColor = seat.classList.add('bg-green-500');
     
-            const seatNo = e.target.innerText;
-            const p1 = document.createElement('p');
-            p1.innerText = seatNo;
-            const containerSeat = document.getElementById('container-seat');
-            containerSeat.appendChild(p1);
-
-            const p2 = document.createElement('p')
-            p2.innerText = 'Economy'
-            const containerClass = document.getElementById('container-class')
-            containerClass.appendChild(p2)
-
-            const p3 = document.createElement('p')
-            p3.innerText = '550'
-            const containerPrice = document.getElementById('container-price')
-            containerPrice.appendChild(p3)
-            
+                const seatNo = e.target.innerText;
+                const p1 = document.createElement('p');
+                p1.innerText = seatNo;
+                const containerSeat = document.getElementById('container-seat');
+                containerSeat.appendChild(p1);
     
-            document.getElementById('seat-count').innerText = count;
-            document.getElementById('seat-left').innerText = 8-count;
-            count++,
+                const p2 = document.createElement('p')
+                p2.innerText = 'Economy'
+                const containerClass = document.getElementById('container-class')
+                containerClass.appendChild(p2)
     
-            document.getElementById('total-price').innerText = totalPrice;
-            document.getElementById('grand-total').innerText = totalPrice;
-            totalPrice += 550;
+                const p3 = document.createElement('p')
+                p3.innerText = '550'
+                const containerPrice = document.getElementById('container-price')
+                containerPrice.appendChild(p3)
+                
+        
+                document.getElementById('seat-count').innerText = count;
+                document.getElementById('seat-left').innerText = 8-count;
+                count++,
+        
+                document.getElementById('total-price').innerText = totalPrice;
+                document.getElementById('grand-total').innerText = totalPrice;
+                totalPrice += 550;
+                
+                count1++;
+            }
+            else{alert('Maximum seat booked')}
         })
     }
-}
-else{
-    alert('Maximum booking completed');
-}
+
 
 // coupon section
 
